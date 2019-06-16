ad4325da0d836a7e0d490d31cdf55f021ca98912
Andrea Bolognani <abologna@redhat.com>
2019-06-16 16:15:37 +0200

    update sample program

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 extern crate virtblocks;
 
+use virtblocks::devices::Memballoon;
+use virtblocks::devices::MemballoonModel;
+
 fn main() {
     let file_name = virtblocks::util::build_file_name("guest", ".xml");
     println!("{}", file_name);
+
+    let mut memballoon = Memballoon::new();
+
+    println!("{}", memballoon.to_str());
+
+    memballoon.set_model(MemballoonModel::VirtIO);
+    println!("{}", memballoon.to_str());
 }
