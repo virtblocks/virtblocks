5604a0f3a871d17b86db27320822ce9fc5e15726
Andrea Bolognani <abologna@redhat.com>
2019-08-05 15:31:25 +0200

    golang: Implement Memballoon device

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
     "fmt"
+    "gitlab.com/virtblocks/virtblocks/devices"
     "gitlab.com/virtblocks/virtblocks/util"
 )
 
 func main() {
     var file_name = util.BuildFileName("guest", ".xml")
     fmt.Println(file_name)
+
+    var memballoon = devices.NewMemballoon()
+    fmt.Println(memballoon.ToStr())
+
+    memballoon.SetModel(devices.MEMBALLOON_MODEL_VIRTIO)
+    fmt.Println(memballoon.ToStr())
 }
