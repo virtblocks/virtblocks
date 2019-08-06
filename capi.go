76b2690230ea5f5e1f70765580b0f8b6a72c263c
Andrea Bolognani <abologna@redhat.com>
2019-08-06 11:30:13 +0200

    golang: Expose devices.Memballoon APIs to C

--- capi.go
+++ capi.go
@@ -3,6 +3,7 @@ package main
 import "C"
 
 import (
+    "gitlab.com/virtblocks/virtblocks/devices"
     "gitlab.com/virtblocks/virtblocks/util"
 )
 
@@ -18,6 +19,38 @@ func virtblocks_util_build_file_name(cFileName **C.char, cBase *C.char, cExt *C.
 
     *cFileName = C.CString(goRes)
     return 0
+}
+
+var memballoons []*devices.Memballoon
+
+//export virtblocks_devices_memballoon_new
+func virtblocks_devices_memballoon_new() C.int {
+    var goMemballoon = devices.NewMemballoon()
+    memballoons = append(memballoons, goMemballoon)
+    return C.int(len(memballoons) - 1)
+}
+
+//export virtblocks_devices_memballoon_free
+func virtblocks_devices_memballoon_free(cMemballoon C.int) {
+    memballoons[cMemballoon] = nil
+}
+
+//export virtblocks_devices_memballoon_set_model
+func virtblocks_devices_memballoon_set_model(cMemballoon C.int, cModel C.int) {
+    var goMemballoon = memballoons[cMemballoon]
+    goMemballoon.SetModel(devices.MemballoonModel(cModel))
+}
+
+//export virtblocks_devices_memballoon_get_model
+func virtblocks_devices_memballoon_get_model(cMemballoon C.int) C.int {
+    var goMemballoon = memballoons[cMemballoon]
+    return C.int(goMemballoon.GetModel())
+}
+
+//export virtblocks_devices_memballoon_to_str
+func virtblocks_devices_memballoon_to_str(cMemballoon C.int) *C.char {
+    var goMemballoon = memballoons[cMemballoon]
+    return C.CString(goMemballoon.ToStr())
 }
 
 func main() {}
