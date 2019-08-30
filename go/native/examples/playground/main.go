c972be1a6af6f15664705403e7719560eabf4504
Andrea Bolognani <abologna@redhat.com>
2019-08-30 11:58:01 +0200

    go/native: Implement playground.Toy object
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

---
+++ go/native/examples/playground/main.go
@@ -0,0 +1,64 @@
+// Virt Blocks
+//
+// Copyright (C) 2019 Red Hat, Inc.
+//
+// This software is distributed under the terms of the MIT License.
+// See the LICENSE file in the top level directory for details.
+
+package main
+
+import (
+	"fmt"
+	"github.com/virtblocks/virtblocks/go/native/pkg/playground"
+)
+
+type Input struct {
+	toy *playground.Toy
+	ext string
+}
+
+// Create an object that already has a *local* closure attached
+func create_baz() *playground.Toy {
+	var ext_is_baz = func(_toy playground.Toy, ext string) bool {
+		return ext == "baz"
+	}
+	return playground.NewToy("baz", ext_is_baz)
+}
+
+// Function to be used as a filter
+func base_and_ext_match(toy playground.Toy, ext string) bool {
+	return toy.Base() == ext
+}
+
+func main() {
+	// Closure to be used as a filter. In this case we're using a local
+	// variable inside the closure
+	var base = "foo"
+	var base_is_foo = func(toy playground.Toy, _ext string) bool {
+		return toy.Base() == base
+	}
+
+	var foo_is_foo_toy = playground.NewToy("foo", base_is_foo)
+	var foo_match_toy = playground.NewToy("foo", base_and_ext_match)
+	var bar_is_foo_toy = playground.NewToy("bar", base_is_foo)
+	var bar_match_toy = playground.NewToy("bar", base_and_ext_match)
+	var baz_toy = create_baz()
+
+	var inputs = [...]Input{
+		Input{toy: foo_is_foo_toy, ext: "exe"},
+		Input{toy: foo_match_toy, ext: "exe"},
+		Input{toy: bar_is_foo_toy, ext: "bar"},
+		Input{toy: bar_match_toy, ext: "bar"},
+		Input{toy: baz_toy, ext: "baz"},
+		Input{toy: baz_toy, ext: "quux"},
+	}
+
+	for _, input := range inputs {
+		res, err := input.toy.Run(input.ext)
+		if err == nil {
+			fmt.Printf("Result: %s\n", res)
+		} else {
+			fmt.Printf("Error: %s\n", err)
+		}
+	}
+}
---
+++ go/native/pkg/playground/toy.go
@@ -0,0 +1,47 @@
+// Virt Blocks
+//
+// Copyright (C) 2019 Red Hat, Inc.
+//
+// This software is distributed under the terms of the MIT License.
+// See the LICENSE file in the top level directory for details.
+
+package playground
+
+const (
+	CallbackFailed = iota
+)
+
+type ToyError int
+
+func (self ToyError) Error() string {
+	var ret string
+
+	switch self {
+	case CallbackFailed:
+		ret = "Callback failed"
+	}
+
+	return ret
+}
+
+type Toy struct {
+	base   string
+	filter func(Toy, string) bool
+}
+
+func NewToy(base string, filter func(Toy, string) bool) *Toy {
+	return &Toy{base: base, filter: filter}
+}
+
+func (self Toy) Base() string {
+	return self.base
+}
+
+func (self Toy) Run(ext string) (string, error) {
+	if self.filter(self, ext) {
+		// If the user-provided filter succeeds, then build a string
+		return self.base + "." + ext, nil
+	} else {
+		return "", ToyError(CallbackFailed)
+	}
+}
