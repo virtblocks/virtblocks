ee114230063599495438d2f97858811fefce17e0
Andrea Bolognani <abologna@redhat.com>
2019-09-02 23:57:20 +0200

    scripts: Update verify-gofmt.sh after recent changes
    Since commit 98c2c28c746b we're calling 'go tool cgo' explicitly,
    and doing so leaves behind a bunch of generated files that don't
    quite satisfy gofmt's extremely high standards when it comes to
    source code beauty.
    
    Change our wrapper script so that it ignores changes to those
    generated files; while at it, also make it less verbose by only
    listing the files that are not formatted properly instead of
    showing the full diff.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- scripts/verify-gofmt.sh
+++ scripts/verify-gofmt.sh
@@ -4,10 +4,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-diff=$(gofmt -d -e ./ 2>&1) || true
+diff=$(gofmt -l ./ 2>&1 | grep -Ev '^_obj/') || true
 if [[ -n "${diff}" ]]; then
+  echo "The following files are not formatted properly:" >&2
+  echo >&2
   echo "${diff}" >&2
   echo >&2
-  echo "Run 'make fmt'" >&2
+  echo "Run 'make fmt' to fix them." >&2
   exit 1
 fi
