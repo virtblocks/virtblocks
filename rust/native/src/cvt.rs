429ee884d808a299d5188879d8f91dfd7f6f4fc8
Andrea Bolognani <abologna@redhat.com>
2019-09-10 22:56:15 +0200

    rust/native: Add authorship information for cvt
    It's not our code, and it should be clearly marked accordingly.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- rust/native/src/cvt.rs
+++ rust/native/src/cvt.rs
@@ -1,3 +1,9 @@
+// Lifted from Rust's src/libstd/sys/unix/mod.rs
+//
+// Rust is licensed under the Apache License, Version 2.0
+// <http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <http://opensource.org/licenses/MIT>, at your option.
+
 use std::io;
 
 #[doc(hidden)]
