a87357e15822555fe1ad6748a7834a85aa90c616
Marc-André Lureau <marcandre.lureau@redhat.com>
2019-09-10 14:10:24 +0400

    rust: fix some closefds issues
    - opendir when actually needed, to avoid TOCTOU
    - use cvt!() macros to handle system errors consistently
    - fix clippy.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

---
+++ rust/native/src/cvt.rs
@@ -0,0 +1,37 @@
+use std::io;
+
+#[doc(hidden)]
+pub trait IsMinusOne {
+    fn is_minus_one(&self) -> bool;
+}
+
+macro_rules! impl_is_minus_one {
+    ($($t:ident)*) => ($(impl IsMinusOne for $t {
+        fn is_minus_one(&self) -> bool {
+            *self == -1
+        }
+    })*)
+}
+
+impl_is_minus_one! { i8 i16 i32 i64 isize }
+
+pub(crate) fn cvt<T: IsMinusOne>(t: T) -> io::Result<T> {
+    if t.is_minus_one() {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(t)
+    }
+}
+
+pub(crate) fn cvt_r<T, F>(mut f: F) -> io::Result<T>
+where
+    T: IsMinusOne,
+    F: FnMut() -> T,
+{
+    loop {
+        match cvt(f()) {
+            Err(ref e) if e.kind() == io::ErrorKind::Interrupted => {}
+            other => return other,
+        }
+    }
+}
