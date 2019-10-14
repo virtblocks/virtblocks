7328c060d1b9bc84d12e192b42f9a4a9f6197f2d
Andrea Bolognani <abologna@redhat.com>
2019-10-14 18:13:33 +0200

    c: Remove #pragma from private.h
    Missed in b2737fb51b56.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- c/private.h
+++ c/private.h
@@ -1,10 +1,13 @@
 /* Copyright (C) 2019 Red Hat, Inc. */
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
-#pragma once
+#ifndef _PRIVATE_H_
+#define _PRIVATE_H_
 
 #include "private_include/command/all.h"
 #include "private_include/devices/all.h"
 #include "private_include/playground/all.h"
 #include "private_include/types/all.h"
 #include "private_include/vm/all.h"
+
+#endif
