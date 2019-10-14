6e9cc3d0b806ceaf4035d6756b915ca16a7db195
Andrea Bolognani <abologna@redhat.com>
2019-10-14 18:14:10 +0200

    c/private_include: Add _ALL_ to guards
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- c/private_include/command/all.h
+++ c/private_include/command/all.h
@@ -1,8 +1,8 @@
 /* Copyright (C) 2019 Red Hat, Inc. */
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
-#ifndef _PRIVATE_COMMAND_H_
-#define _PRIVATE_COMMAND_H_
+#ifndef _PRIVATE_COMMAND_ALL_H_
+#define _PRIVATE_COMMAND_ALL_H_
 
 #include "command.h"
 
