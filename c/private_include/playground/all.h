5f70898268658c33726ae2b6844770fead525e3f
Andrea Bolognani <abologna@redhat.com>
2019-10-14 18:14:10 +0200

    c/private_include: Add _ALL_ to guards
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- c/private_include/playground/all.h
+++ c/private_include/playground/all.h
@@ -1,8 +1,8 @@
 /* Copyright (C) 2019 Red Hat, Inc. */
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 
-#ifndef _PRIVATE_PLAYGROUND_H_
-#define _PRIVATE_PLAYGROUND_H_
+#ifndef _PRIVATE_PLAYGROUND_ALL_H_
+#define _PRIVATE_PLAYGROUND_ALL_H_
 
 #include "toy.h"
 
